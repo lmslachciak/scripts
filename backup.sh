0a8c3a570314009e5d2d2f0bbd5e581b66b4999f
Lukasz Slachciak <l.slachciak@list.pl>
2012-09-15 13:37:59 +0200

    Updated backup script for correct Backup path

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 echo "===================="
 date
-mkdir -p $HOME/backup
+mkdir -p $HOME/Backup
 date=`date +%Y.%m.%d`
 for d in .ssh .thunderbird .mozilla
 do
     echo Backing up $HOME/$d to $HOME/backup/$d-$date.tar.gz
-    tar -C $HOME -czf $HOME/backup/$d-$date.tar.gz $d
+    tar -C $HOME -czf $HOME/Backup/$d-$date.tar.gz $d
 done
 date
 echo "===================="
