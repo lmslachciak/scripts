8f88d7d58a6faecceb770e02a9cbf6117ac0640b
Lukasz Slachciak <l.slachciak@samsung.com>
2012-09-26 08:27:54 +0200

    Fixed backup echo

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ mkdir -p $HOME/Backup
 date=`date +%Y.%m.%d`
 for d in .ssh .thunderbird .mozilla
 do
-    echo Backing up $HOME/$d to $HOME/backup/$d-$date.tar.gz
+    echo Backing up $HOME/$d to $HOME/Backup/$d-$date.tar.gz
     tar -C $HOME -czf $HOME/Backup/$d-$date.tar.gz $d
 done
 date
