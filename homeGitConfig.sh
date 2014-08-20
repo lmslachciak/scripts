402dc05be2408c31482cc783a442c0bdb9785606
Lukasz Slachciak <lslachciak@home.pl>
2014-08-20 22:38:22 +0200

    e-mail update

--- homeGitConfig.sh
+++ homeGitConfig.sh
@@ -1,4 +1,4 @@
 git config --global color.ui true
-git config --global user.email "l.slachciak@list.pl"
+git config --global user.email "lslachciak@home.pl"
 git config --global user.name "Lukasz Slachciak"
 git config --global credential.helper 'cache --timeout=3600'
