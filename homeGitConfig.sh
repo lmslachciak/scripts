2a606debaf8554ce384f5dcb574115d8573cb171
Lukasz Slachciak <lukasz@fastmail.com>
2015-02-19 22:10:51 +0100

    updated e-mail address

--- homeGitConfig.sh
+++ homeGitConfig.sh
@@ -1,4 +1,4 @@
 git config --global color.ui true
-git config --global user.email "lslachciak@home.pl"
+git config --global user.email "lukasz@fastmail.com"
 git config --global user.name "Lukasz Slachciak"
 git config --global credential.helper 'cache --timeout=3600'
