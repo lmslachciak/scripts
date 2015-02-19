a491d59cf382fc5feecccbd0485a9a72f0435036
Lukasz Slachciak <lukasz@fastmail.com>
2015-02-19 22:12:57 +0100

    Credential helper added to git configuration script

--- homeGitConfig.sh
+++ homeGitConfig.sh
@@ -1,4 +1,5 @@
 git config --global color.ui true
 git config --global user.email "lukasz@fastmail.com"
 git config --global user.name "Lukasz Slachciak"
+git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
